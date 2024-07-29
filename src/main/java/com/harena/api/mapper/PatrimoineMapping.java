06faac1f03877405e87552a3e7d790e33b544d36
RotsyMandrindra <rotsy.rasoanaivo@gmail.com>
2024-07-29 10:29:14 +0300

    Revert "Revert "Revert "Revert "refactor code""""
    This reverts commit b1d20df5851d1e23766583ef7a60c0fca4e04d4c.

--- src/main/java/com/harena/api/mapper/PatrimoineMapping.java
+++ src/main/java/com/harena/api/mapper/PatrimoineMapping.java
@@ -4,15 +4,16 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import school.hei.patrimoine.modele.Patrimoine;
 
-import java.util.Set;
-
 @Component
 @AllArgsConstructor
-
 public class PatrimoineMapping {
-    private final PersonneMapping personneMapping;
+  private final PersonneMapping personneMapping;
 
-    public Patrimoine transformToRestModel(Patrimoine patrimoine) {
-        return new Patrimoine(patrimoine.nom(), personneMapping.transformToRestModel(patrimoine.possesseur()), patrimoine.t(), patrimoine.possessions());
-    }
+  public Patrimoine transformToRestModel(Patrimoine patrimoine) {
+    return new Patrimoine(
+        patrimoine.nom(),
+        personneMapping.transformToRestModel(patrimoine.possesseur()),
+        patrimoine.t(),
+        patrimoine.possessions());
+  }
 }
